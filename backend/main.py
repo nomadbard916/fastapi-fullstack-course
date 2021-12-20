798b03bc26233dfcecef55ed4d01577e9130f57b
Kevin Hou <neonnova916@gmail.com>
2021-12-20 22:24:41 +0800

    create users table and base class

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
-from db.base_class import Base
+from db.base import Base
+
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
@@ -12,8 +14,4 @@ def start_application():
     return app
 
 
-
-
 app = start_application()
-
-
