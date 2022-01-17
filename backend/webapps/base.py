d9b28a79e451e1ab4d96573eecc50b107ea0d1ab
Kevin Hou <neonnova916@gmail.com>
2022-01-17 20:04:05 +0800

    user authentication

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 from .jobs import route_jobs
 from .users import route_users
-
-# from webapps.auth import route_login
+from .auth import route_login
 
 api_router = APIRouter()
 
 api_router.include_router(route_jobs.router, prefix="", tags=["homepage"])
 api_router.include_router(route_users.router, prefix="", tags=["users"])
-# api_router.include_router(route_login.router, prefix="", tags=["auth"])
+api_router.include_router(route_login.router, prefix="", tags=["auth"])
